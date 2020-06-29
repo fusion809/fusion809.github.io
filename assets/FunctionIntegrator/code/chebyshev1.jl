c4541e08c23dec5c0f476a0498da4fe301bc832d
fusion809 <brentonhorne77@gmail.com>
2020-06-29 23:59:47 +0000

    Deploying to master from  @ 53467a1eaf893e8d2c1f4bf53c29e29dfb031c02 🚀

--- assets/FunctionIntegrator/code/chebyshev1.jl
+++ assets/FunctionIntegrator/code/chebyshev1.jl
@@ -1,6 +1,4 @@
 # This file was generated, do not modify it. # hide
-using Pkg;
-Pkg.add("FunctionIntegrator")
 using FunctionIntegrator
 a = abs(chebyshev_quadrature(x -> cos(x), 1000, 1, 0, pi/2) - 1);
 show(a)
