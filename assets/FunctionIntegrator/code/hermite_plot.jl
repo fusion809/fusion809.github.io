af9d2446f25b91778e82032b0c8900fecc895607
fusion809 <brentonhorne77@gmail.com>
2020-06-30 08:59:54 +0000

    Deploying to master from  @ 5149bd8a8bdf16a6dfd262b19a4d242f8c36a17d 🚀

--- assets/FunctionIntegrator/code/hermite_plot.jl
+++ assets/FunctionIntegrator/code/hermite_plot.jl
@@ -4,4 +4,4 @@ x = LinRange(-10,10,1001);
 clf()
 y = (x.^2).*exp.(-x.^2);
 PyPlot.plot(x,y)
-PyPlot.savefig(joinpath(@OUTPUT, "hermite_plot.png"), dpi=50)
+PyPlot.savefig(joinpath(@OUTPUT, "hermite_plot.png"), dpi=80)
--- assets/FunctionIntegrator/code/laguerre_plot.jl
+++ assets/FunctionIntegrator/code/laguerre_plot.jl
@@ -4,4 +4,4 @@ x = LinRange(0,15,1001);
 clf()
 y = x.*exp.(-x);
 PyPlot.plot(x,y)
-PyPlot.savefig(joinpath(@OUTPUT, "laguerre_plot.png"), dpi=50)
+PyPlot.savefig(joinpath(@OUTPUT, "laguerre_plot.png"), dpi=80)
