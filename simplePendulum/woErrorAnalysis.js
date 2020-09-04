fd6168d4525d8d4f545432cced21fcb4410c182e
Brenton Horne <brentonhorne77@gmail.com>
2020-09-04 17:37:13 +1000

    rm global vars from w[o]ErrorAnalysis, also modularizing funcs

--- simplePendulum/woErrorAnalysis.js
+++ simplePendulum/woErrorAnalysis.js
@@ -1,29 +1,53 @@
 /**
- * Right-hand side of our second-order ODE written as a simple of first-order
- * ODEs.
+ * Reads inputs in the parameters table, adds them to an array and return
+ * 
+ * @params         None.
+ * @return         Array of problem parameters.
+ */
+function readInputs() {
+    var g = parseFloat(document.getElementById("g").value);
+    var l = parseFloat(document.getElementById("l").value);
+    var N = parseFloat(document.getElementById("N").value);
+    var theta0 = parseFloat(document.getElementById("theta0").value);
+    var thetaDot0 = parseFloat(document.getElementById("thetaDot0").value);
+    var thetaMaxInitial = parseFloat(document.getElementById("thetaMaxInitial").value);
+    var xi = parseFloat(document.getElementById("xi").value);
+    var epsilon = parseFloat(document.getElementById("epsilon").value);
+    var dtInitial = parseFloat(document.getElementById("dtInitial").value);
+    var t0 = parseFloat(document.getElementById("t0").value);
+    var tf = parseFloat(document.getElementById("tf").value);
+
+    return [g, l, N, theta0, thetaDot0, thetaMaxInitial, xi, epsilon, dtInitial, t0, tf];
+
+    // g:                 arrayOfInputs[0]
+    // l:                 arrayOfInputs[1]
+    // N:                 arrayOfInputs[2]
+    // theta0:            arrayOfInputs[3]
+    // thetaDot0:         arrayOfInputs[4]
+    // thetaMaxInitial:   arrayOfInputs[5]
+    // xi:                arrayOfInputs[6]
+    // epsilon:           arrayOfInputs[7]
+    // dtInitial:         arrayOfInputs[8]
+    // t0:                arrayOfInputs[9]
+    // tf:                arrayOfInputs[10]
+}
+
+/**
+ * Right-hand side of our second-order ODE written as a system of 
+ * first-order ODEs.
  *
- * @param g          Acceleration due to gravity in metres per second squared.
+ * @param g          Acceleration due to gravity in metres per second 
+ * squared.
  * @param l          Length of the pendulum in metres.
- * @param t          Time (seconds).
- * @param theta      Angle of the pendulum from the positive x-axis (in
- * radians)
- * @param thetaDot   Rate of change against time of the angle from the positive x-axis.
+ * @param t          Time.
+ * @param theta      Angle of the pendulum from the positive x-axis.
+ * @param thetaDot   Rate of change against time of the angle from the 
+ * positive x-axis.
  * @return           [dtheta/dt, d2theta/dt2]
  */
 function f(g, l, t, theta, thetaDot) {
     return [thetaDot, -g/l*Math.cos(theta)];
-}
-
-// Initialize our global variables
-var solution = {
-    t: [],
-    theta: [],
-    thetaDot: []
 };
-var windowInnerWidth;
-var windowInnerHeight;
-var epsilon, N, g, l, theta0, thetaDot0, thetaMax, thetaMaxInitial, adjMin, adjMax, j, k;
-var integral = 0;
 
 /**
  * Returns the value of theta dot squared.
@@ -31,7 +55,7 @@ var integral = 0;
  * @param theta    Angle from the x-axis.
  * @return         Theta dot squared value.
  */
-function thetaDotSq(theta) {
+function thetaDotSq(g, l, theta0, thetaDot0, theta) {
     return thetaDot0**2 + 2*g/l * (Math.sin(theta0)-Math.sin(theta));
 }
 
@@ -41,8 +65,8 @@ function thetaDotSq(theta) {
  * @param theta    Angle from the x-axis.
  * @return         Theta derivative of the theta dot squared expression.
  */
-function thetaDotSqPrime(theta) {
-    return -2*g/l * Math.cos(theta)
+function thetaDotSqPrime(g, l, theta) {
+    return -2*g/l * Math.cos(theta);
 }
 
 /**
@@ -52,8 +76,76 @@ function thetaDotSqPrime(theta) {
  * @return         Correction to theta min/max value according to Newton's
  * method.
  */
-function newtonsCorrection(theta) {
-    return -thetaDotSq(theta)/thetaDotSqPrime(theta);
+function newtonsCorrection(g, l, theta0, thetaDot0, theta) {
+    return -thetaDotSq(g, l, theta0, thetaDot0, theta)/thetaDotSqPrime(g, l, theta);
+}
+
+/**
+ * Calculate the bounds of theta in our integration
+ * 
+ * @param arrayOfInputs      An array of problem parameters.
+ * @return                   Nothing. Just writes the relevant  
+ */
+function thetaBounds(arrayOfInputs) {
+    // Parameters of the problem that are necessary to calculate the period
+    var g = arrayOfInputs[0];
+    var l = arrayOfInputs[1];
+    var N = arrayOfInputs[2];
+    var theta0 = arrayOfInputs[3];
+    var thetaDot0 = arrayOfInputs[4];
+    var thetaMaxInitial = arrayOfInputs[5];
+    var xi = arrayOfInputs[6];
+
+    // Take our initial guess for thetaMax
+    var thetaMax = thetaMaxInitial;
+    var thetaMin = theta0;
+    // thetaMax correction
+    var adjMax = newtonsCorrection(g, l, theta0, thetaDot0, thetaMax);
+    var adjMin;
+
+    // Calculate when thetaDot = 0 next, which will be halfway through the problem's period
+    var j = 0; 
+    var k = 0;
+    while ( ( Math.abs(adjMax) >= xi) && ( j < N )) {
+        thetaMax += adjMax;
+        adjMax = newtonsCorrection(g, l, theta0, thetaDot0, thetaMax);
+        j++;
+    }
+    thetaMax += adjMax;
+
+    // Calculate thetaMin, which is when thetaDot = 0
+    if (thetaDot0 != 0) {
+        adjMin = newtonsCorrection(g, l, theta0, thetaDot0, thetaMin);    
+        while (( Math.abs(adjMin) >= xi) & (k < N)) {
+            thetaMin += adjMin;
+            adjMin = newtonsCorrection(g, l, theta0, thetaDot0, thetaMin);
+            k++;
+        }
+        thetaMin += adjMin;
+    } else {
+        thetaMin = theta0;
+    }
+
+    // If j and k reach N mention that in the table to let the user know
+    // that the maximum iterations were reached
+    if ( (j >= N ) || (k >= N ) ) {
+        document.getElementById("integralDisplay").innerHTML = "Theta min and/or theta max could not be calculated as the limit on Newton's method iterations was exceeded";
+        document.getElementById("tf").value = 100;
+    }
+
+    // Mention theta bounds and Newton's info in data table
+    document.getElementById("jDisplay").innerHTML = j;
+    document.getElementById("kDisplay").innerHTML = k;
+    document.getElementById("thetaMinDisplay").innerHTML = thetaMin;
+    document.getElementById("thetaMaxDisplay").innerHTML = thetaMax;
+    document.getElementById("adjMinDisplay").innerHTML = adjMin;
+    document.getElementById("adjMaxDisplay").innerHTML = adjMax;
+
+    // Add thetaMin and thetaMax to arrayOfInputs
+    arrayOfInputs.push(thetaMin);
+    arrayOfInputs.push(thetaMax);
+
+    return arrayOfInputs;
 }
 
 /**
@@ -64,48 +156,15 @@ function newtonsCorrection(theta) {
  * @return         Nothing. Changes the element value/innerHTML of 
  * integralDisplay and tf to T and 4*T, respectively.
  */
-function periodCalc() {
+function periodCalc(arrayOfInputs) {
     // Parameters of the problem that are necessary to calculate the period
-    g = parseFloat(document.getElementById("g").value);
-    l = parseFloat(document.getElementById("l").value);
-    N = parseFloat(document.getElementById("N").value);
-    theta0 = parseFloat(document.getElementById("theta0").value);
-    thetaDot0 = parseFloat(document.getElementById("thetaDot0").value);
-    thetaMaxInitial = parseFloat(document.getElementById("thetaMaxInitial").value);
-    var xi = parseFloat(document.getElementById("xi").value);
-
-    // Take our initial guess for thetaMax
-    var thetaMax = thetaMaxInitial;
-    // thetaMax correction
-    adjMax = newtonsCorrection(thetaMax);
-
-    // Calculate when thetaDot = 0 next, which will be halfway through the problem's period
-    j = 0; 
-    k = 0;
-    while ( ( Math.abs(adjMax) >= xi) && ( j < N )) {
-        thetaMax += adjMax;
-        adjMax = newtonsCorrection(thetaMax);
-        j++;
-    }
-
-    // Calculate thetaMin, which is when thetaDot = 0
-    if (thetaDot0 != 0) {
-        var thetaMin = theta0;
-        adjMin = newtonsCorrection(thetaMin);    
-        while (( Math.abs(adjMin) >= xi) & (k < N)) {
-            thetaMin += adjMin;
-            adjMin = newtonsCorrection(thetaMin);
-            k++;
-        }
-    } else {
-        thetaMin = theta0;
-    }
-
-    if ( (j >= N ) || (k >= N ) ) {
-        document.getElementById("integralDisplay").innerHTML = "Theta min and/or theta max could not be calculated due as the limit on Newton's method iterations was exceeded";
-        document.getElementById("tf").value = 100;
-        return
-    }
+    var g = arrayOfInputs[0];
+    var l = arrayOfInputs[1];
+    var N = arrayOfInputs[2];
+    var theta0 = arrayOfInputs[3];
+    var thetaDot0 = arrayOfInputs[4];
+    var thetaMin = arrayOfInputs[11];
+    var thetaMax = arrayOfInputs[12];
 
     // Integrate problem from thetaMin to thetaMax to calculate the period
     // in seconds
@@ -116,7 +175,7 @@ function periodCalc() {
     for ( let i = 1; i < N+1; i++) {
         nodes = Math.cos((2*i-1)*Math.PI/(2*N));
         transformedGrid = (thetaMax-thetaMin)*nodes/2+(thetaMax+thetaMin)/2;
-        integrand = Math.sqrt(1-nodes**2)*Math.pow(thetaDotSq(transformedGrid),-1/2);
+        integrand = Math.sqrt(1-nodes**2)*Math.pow(thetaDotSq(g, l, theta0, thetaDot0, transformedGrid),-1/2);
         integral += ((thetaMax-thetaMin)/2) * (Math.PI/N)*integrand;
     }
     period = 2*Math.abs(integral);
@@ -124,29 +183,24 @@ function periodCalc() {
     // Change what's displayed on the page accordingly
     document.getElementById("integralDisplay").innerHTML = period;
     document.getElementById("tf").value = 4*period;
-    document.getElementById("jDisplay").innerHTML = j;
-    document.getElementById("kDisplay").innerHTML = k;
-    document.getElementById("adjMinDisplay").innerHTML = adjMin;
-    document.getElementById("adjMaxDisplay").innerHTML = adjMax;
-    document.getElementById("thetaMinDisplay").innerHTML = thetaMin;
-    document.getElementById("thetaMaxDisplay").innerHTML = thetaMax;
+    return period;
 }
 
 /** 
  * Solve the problem using RK45.
  *
  * @params           None. Uses parameter values in the forum.
- * @return           Nothing. But it enters the solution values into the solution
- * object.
+ * @return           Nothing. But it enters the solution values and error estimates
+ * into the solution object.
  */
-function solveProblem() {
+function solveProblem(arrayOfInputs) {
     // Obtain the parameters of the problem
-    g = parseFloat(document.getElementById("g").value);
-    l = parseFloat(document.getElementById("l").value);
-    t0 = parseFloat(document.getElementById("t0").value);
-    tf = parseFloat(document.getElementById("tf").value);
-    theta0 = parseFloat(document.getElementById("theta0").value);
-    thetaDot0 = parseFloat(document.getElementById("thetaDot0").value);
+    g = arrayOfInputs[0]
+    l = arrayOfInputs[1];
+    t0 = arrayOfInputs[9];
+    tf = arrayOfInputs[10];
+    theta0 = arrayOfInputs[3];
+    thetaDot0 = arrayOfInputs[4];
     epsilon = parseFloat(document.getElementById("epsilon").value);
     dtInitial = parseFloat(document.getElementById("dtInitial").value);
 
@@ -162,8 +216,8 @@ function solveProblem() {
         // Step size, as dictated by the method
         dt = Math.min(dt, tf-t[i]);
 
-        // Runge-Kutta-Fehlberg approximations of the change in theta and thetaDot
-        // over the step
+        // Runge-Kutta-Fehlberg approximations of the change in theta and 
+        // thetaDot over the step
         k1 = dt*f(g, l, t[i], theta[i], thetaDot[i])[0];
         l1 = dt*f(g, l, t[i], theta[i], thetaDot[i])[1];
         k2 = dt*f(g, l, t[i]+dt/4, theta[i]+k1/4, thetaDot[i]+l1/4)[0];
@@ -195,45 +249,46 @@ function solveProblem() {
             t.push(t[i]+dt);
             theta.push(theta1);
             thetaDot.push(thetaDot1);
-            i++;
             dt *= s;
+            i++;
         } else {
             dt *= s;
         }
     }
 
-    // Write t, theta and thetaDot to our solution object
-    solution = {
+    // Write our arrays to the solution object
+    var solution = {
         t: t,
         theta: theta,
-        thetaDot: thetaDot,
+        thetaDot: thetaDot
     };
 
     // Write number of steps to table field
     document.getElementById("NRKF45").innerHTML = t.length;
-}
+
+    return solution;
+};
 
 /**
  * Tabulates solution data.
  *
- * @params           None. Uses the entries of the solution object, however. 
+ * @params           None.
  * @return           Nothing. Just populates the table with the solution values. 
  */
-function fillTable() {
-    if ( solution["t"].length == 0) {
-        alert('You haven\'t solved the problem yet! Press the "Solve the problem" button before pressing the "Tabulate the solution" button again.');
-        return
-    }
-    t = solution["t"];
-    theta = solution["theta"];
-    thetaDot = solution["thetaDot"];
+function fillTable(arrayOfInputs) {
+    var solution = solveProblem(arrayOfInputs);
+    var t = solution["t"];
+    var theta = solution["theta"];
+    var thetaDot = solution["thetaDot"];
+    
+    // Generate table
     document.getElementById('tableOutputs').innerHTML = '';
     tableContents = '<tr>';
     tableContents += '<th>Index</th>';
     tableContents += '<th>t (seconds)</th>';
     tableContents += '<th>&theta; (radians) </th>';
-    tableContents += '<th>&theta; dot <br/>(radians &middot; s<sup>-1</sup>)</th>';
-    tableContents += "</tr>";
+    tableContents += '<th>&theta; dot<br/>(radians &middot; s<sup>-1</sup>)</th>';
+    tableContents += '</tr>';
     for (let i = 0; i < theta.length; i++) {
         tableContents += '<tr>';
         tableContents += '<td>' + i + '</td>';
@@ -243,7 +298,7 @@ function fillTable() {
         tableContents += '</tr>';
     }
     document.getElementById('tableOutputs').innerHTML = tableContents;
-}
+};
 
 /**
  * Removes the solution table
@@ -261,19 +316,16 @@ function removeTable() {
  * @params           None.
  * @return           Nothing. Just generates the relevant plot.
  */
-function generatePhasePlot() {
+function generatePhasePlot(arrayOfInputs) {
     // Run solveProblem() if previously unrun
-    if ( solution["t"].length == 0) {
-        solveProblem();
-    };
+    var solution = solveProblem(arrayOfInputs);
 
     // Extract solution data from solution object
-    theta = solution["theta"];
-    thetaDot = solution["thetaDot"];
+    var theta = solution["theta"];
+    var thetaDot = solution["thetaDot"];
 
     // Height and width of plot
-    windowInnerWidth  = window.innerWidth;
-    windowInnerHeight = window.innerHeight;
+    var windowInnerHeight = window.innerHeight;
     document.getElementById("phasePlot").style = "height: " + windowInnerHeight + "px;";
 
     // Characteristics of the phase plot
@@ -315,20 +367,16 @@ function removePhasePlot() {
  * @params           None.
  * @return           Nothing. Just generates the relevant plot.
  */
-function generateTimePlot() {
-    // Run solveProblem() if previously unrun
-    if ( solution["t"].length == 0) {
-        solveProblem();
-    };
+function generateTimePlot(arrayOfInputs) {
+    var solution = solveProblem(arrayOfInputs);
 
     // Extract solution data from solution object
-    t = solution["t"];
-    theta = solution["theta"];
-    thetaDot = solution["thetaDot"];
+    var t = solution["t"];
+    var theta = solution["theta"];
+    var thetaDot = solution["thetaDot"];
 
     // Height and width of plots
-    windowInnerWidth  = window.innerWidth;
-    windowInnerHeight = window.innerHeight;
+    var windowInnerHeight = window.innerHeight;
     document.getElementById("timePlot").style = "height: " + windowInnerHeight + "px;";
 
     // Characteristics of the theta and theta dot against time plot
@@ -369,19 +417,19 @@ function removeTimePlot() {
 
 /**
  * Generate two plots:
- * - one of thetaDot and theta against t; and
+ * - one of thetaDot and theta against t;
  * - a phase plot of thetaDot against theta.
  * 
  * @params           None.
  * @return           Nothing. Just generates the plots.
  */
-function generatePlots() {
-    generateTimePlot();
-    generatePhasePlot();
+function generatePlots(arrayOfInputs) {
+    generateTimePlot(arrayOfInputs);
+    generatePhasePlot(arrayOfInputs);
 };
 
 /**
- * Removes solution plots
+ * Removes solution plots and associated element formatting
  * 
  * @params           None.
  * @return           Nothing. Just removes the solution plots.
@@ -389,6 +437,6 @@ function generatePlots() {
 function removePlots() {
     document.getElementById("timePlot").innerHTML = '';
     document.getElementById("phasePlot").innerHTML = '';
-    document.getElementById("timePlot").style = '';
+    document.getElementById("timePlot").style = ''
     document.getElementById("phasePlot").style = '';
-};
+}
