4d493a9340c995d8026d00be9bf9cfccfca352bb
fusion809 <brentonhorne77@gmail.com>
2020-09-26 16:27:18 +0000

    Deploying to master from  @ c76aab841df0e28e91ac66474485a232e5205e75 🚀

--- libs/common/arrays.js
+++ libs/common/arrays.js
@@ -1,25 +1,15 @@
 /**
- * Add 1d arrays contained with a 2d array
+ * Add arrays
+ * Previously used an iterative algorithm.
+ * This code is from StackOverflow https://stackoverflow.com/a/55533058/1876983.
  * 
- * @param arr      A 2d array with each column being a different array to be added.
- * @return         1d array whose elements are the sum of corresponding elements of the 1d subarrays of arr.
+ * @param arrs     A list of arrays to be added
+ * @return         Array containing elements summed from the input arrays.
  */
-function arrAdd(arr) {
-    // Initialize relevant variables
-    var finalArr = new Array(n1);
-    var n1 = arr.length;
-    var n2 = arr[0].length;
-
-    // Add elements across each column of arr
-    for (let i = 0; i < n2 ; i++) {
-        finalArr[i] = arr[0][i];
-        for (let j = 1; j < n1; j++) {
-            finalArr[i] += arr[j][i];
-        }
-    }
-
-    // Return the column vector of values
-    return finalArr;
+function arrAdd(...arrs) {
+    const n = arrs.reduce((max, xs) => Math.max(max, xs.length), 0);
+    const result = Array.from({ length: n });
+    return result.map((_, i) => arrs.map(xs => xs[i] || 0).reduce((sum, x) => sum + x, 0));
 }
 
 /**
@@ -40,14 +30,6 @@ function arrDiv(arr, scalar) {
  * @param scalar   Scalar by which array is to be multiplied.
  */
 function arrMult(arr, scalar) {
-    // Initialize relevant variables
-    var n = arr.length;
-    var finalArr = new Array(n);
-
-    // Multiply each element by scalar
-    for (let j = 0; j < n; j++) {
-        finalArr[j] = arr[j]*scalar;
-    }
-
-    return finalArr;
+    arr = arr.map(x => x*scalar);
+    return arr;
 }
