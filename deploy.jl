6b4d1af1efdd8564f5c0db208ffc99d3f9ed9739
fusion809 <brentonhorne77@gmail.com>
2025-07-18 09:44:23 +0000

    Deploying to gh-pages from  @ 13011eac21791345b0dbe53b0318621c2f4e5a84 🚀

--- deploy.jl
+++ deploy.jl
@@ -1,4 +1,6 @@
+using Franklin;
 include("utils.jl")
+println("Running deploy.jl!")
 
 function eager_generate_js()
   md_dir = joinpath(@__DIR__, ".")
@@ -8,11 +10,10 @@ function eager_generate_js()
       if endswith(file, ".md")
         md_path = joinpath(root, file)
         content = read(md_path, String)
-
+        println("md_path = " * md_path)
         title = match(r"title *= *\"([^\"]+)\"", content)
         type  = match(r"type *= *\"([^\"]+)\"", content)
         vars  = match(r"vars *= *\[([^\]]+)\]", content)
-
         if isnothing(type) || isnothing(vars)
           continue
         end
@@ -21,6 +22,9 @@ function eager_generate_js()
         page_title = isnothing(title) ? "Untitled" : title.captures[1]
         page_type  = type.captures[1]
         page_vars  = split(vars.captures[1], r"\s*,\s*")
+        println("page_title = " * page_title)
+        println("page_type = " * page_type)
+        println("page_vars[1] = " * page_vars[1])
 
         try
           render_js(page_title, page_type, page_vars)
